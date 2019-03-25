db386089f04ee51b1491669f73442deb7494f74a
Nadiafa <nadiafa581@gmail.com>
2019-03-25 16:34:54 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,11 @@ class ApplicationController < ActionController::Base
   private
 
     def authenticate_user
-      redirect_to "https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_CLIENT']}&scope=repo" if !logged_in?
+      if !logged_in?
+        redirect_to "https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_CLIENT']}&scope=repo" 
+      else
+        @github = GithubService.new({ 'access_token' => session[:token] })
+      end
     end
 
     def logged_in?
